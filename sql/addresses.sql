31741ee6ee62c5af92b55196a432246b848af9fa
fengnan <176560418@qq.com>
2018-09-22 21:42:03 +0800

    add addresses.sql

---
+++ sql/addresses.sql
@@ -0,0 +1,9 @@
+CREATE TABLE `addresses`(
+`id` bigint(20) NOT NULL,
+`user_id` bigint(20) NOT NULL,
+`address` varchar(255) DEFAULT NULL,
+`selected` tinyint(1) DEFAULT NULL,
+`removed` tinyint(1) DEFAULT 0,
+PRIMARY KEY (`id`),
+FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=utf8;
